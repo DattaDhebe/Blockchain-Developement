ec69ed554df76664c8925df98475785aaf29cb90
DattaDhebe <dattadhebe75@gmail.com>
2021-02-09 16:59:56 +0530

    Create Blockchain irst Genesis Block

---
+++ Module 1-Create Blockchain/blockchain.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 15:15:30 2021
+
+@author: Datta
+"""
+# To install Flask : pip install Flask==0.12.2
+# Module 1- Create a Blockchain
+# Import the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# part 1 - Building a Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0') 
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+        
+
+
+# part 2 - Mining our Blockchain
