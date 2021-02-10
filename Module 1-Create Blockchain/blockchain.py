c0287fc7fc39e0380aca3731318a7cbed2d8ab33
DattaDhebe <dattadhebe75@gmail.com>
2021-02-10 14:22:05 +0530

    Added Function check if chain is valid.

--- Module 1-Create Blockchain/blockchain.py
+++ Module 1-Create Blockchain/blockchain.py
@@ -49,5 +49,28 @@ class Blockchain:
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
+    # find if chain is valid 
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2)
+                                            .encode()).hexdigest()
+            # check if '0000'
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+              
+            
+            
+        
+    
     
 # part 2 - Mining our Blockchain
