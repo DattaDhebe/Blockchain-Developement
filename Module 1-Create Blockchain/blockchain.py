ceaec3812ff88ae9cc461ee43465b8b52d140486
DattaDhebe <dattadhebe75@gmail.com>
2021-02-10 21:06:38 +0530

    Added Mining a Block Function for Mining.

--- Module 1-Create Blockchain/blockchain.py
+++ Module 1-Create Blockchain/blockchain.py
@@ -74,3 +74,24 @@ class Blockchain:
     
     
 # part 2 - Mining our Blockchain
+
+# Create a Web App
+app = Flask(__name__)
+
+# Mining a new block
+blockchain = Blockchain()
+
+# Mining new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congrolations, ou just mined a block!',
+                'index': block['proof'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
