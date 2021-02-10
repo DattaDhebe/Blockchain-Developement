0a5c267e6560f036eb1daeeb43badd859abb5dd8
DattaDhebe <dattadhebe75@gmail.com>
2021-02-10 21:22:42 +0530

    Added Function to getting full blockchain.

--- Module 1-Create Blockchain/blockchain.py
+++ Module 1-Create Blockchain/blockchain.py
@@ -89,9 +89,16 @@ def mine_block():
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'Congrolations, ou just mined a block!',
+    response = {'message': 'Congralutions, ou just mined a block!',
                 'index': block['proof'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
                 'previous_hash': block['previous_hash']}
-    return jsonify(response), 200
+    return jsonify(response), 200 # ok http request
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200 # ok http request
